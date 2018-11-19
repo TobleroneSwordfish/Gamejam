7462d571ed128c8f892a453f9d44d047d4aa5e75
TobleroneSwordfish <jonathan.hall859@gmail.com>
2018-11-19 13:57:52 +0000

    Added door and broke Sharlene's meta files sowwy :(

---
+++ Unity bit/Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class Door : MonoBehaviour
+{
+    public GameObject input;
+    public FirstPersonController controller;
+
+    // Update is called once per frame
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            input.SetActive(true);
+            controller.enabled = false;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            input.SetActive(false);
+            controller.enabled = true;
+        }
+    }
+}
