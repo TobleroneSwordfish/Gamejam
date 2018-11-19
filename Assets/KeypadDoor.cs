d3e2a24c9150fd45e1fc426f25e45c8bc8d09e54
TobleroneSwordfish <jonathan.hall859@gmail.com>
2018-11-19 22:34:08 +0000

    Keypad door

---
+++ Assets/KeypadDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadDoor : Door {
+    public GameObject buttonParent;
+    public string passCode;
+    private KeypadItem[] buttons;
+	// Use this for initialization
+	void Start () {
+        buttons = buttonParent.GetComponentsInChildren<KeypadItem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        string value = "";
+		foreach (KeypadItem button in buttons)
+        {
+            value += button.Value.ToString();
+        }
+        if (value == passCode)
+        {
+            OpenDoor();
+        }
+	}
+}
---
+++ Assets/KeypadItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadItem : MonoBehaviour
+{
+    public TextMesh text;
+    public int Value { get { return text.text[0] - '0'; } }
+    private void OnMouseDown()
+    {
+        print("number clicked");
+        if (Input.GetMouseButton(0))
+        {
+            int digit = text.text[0] - '0';
+            if (digit < 9)
+            {
+                text.text = (digit + 1).ToString();
+            }
+            else
+            {
+                text.text = "0";
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
