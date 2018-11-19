291dbe4b77b96c92ba8faaaf1b6117159f5a21a3
TobleroneSwordfish <jonathan.hall859@gmail.com>
2018-11-19 14:24:22 +0000

    Trying to fix gitignore

--- Unity bit/Assets/Door.cs
+++ Unity bit/Assets/Door.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
 public class Door : MonoBehaviour
 {
-    public GameObject input;
+    public GameObject UI;
     public FirstPersonController controller;
-
+    public InputField field;
+    public bool uiOpen = false;
     // Update is called once per frame
     private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !uiOpen)
         {
-            input.SetActive(true);
+            UI.SetActive(true);
             controller.enabled = false;
+            //Cursor.lockState = CursorLockMode.Confined;
+            field.ActivateInputField();
+            uiOpen = true;
         }
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && uiOpen)
         {
-            input.SetActive(false);
+            UI.SetActive(false);
             controller.enabled = true;
+            //Cursor.lockState = CursorLockMode.Locked;
+            uiOpen = false;
         }
     }
 }
