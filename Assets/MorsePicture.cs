955730980f7ff4b5260ec952debed996be37bf41
Sharlene <sd17085@my.bristol.ac.uk>
2018-11-20 04:59:15 +0000

    Stuff

---
+++ Assets/MorsePicture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorsePicture : MonoBehaviour {
+    // Use this for initialization
+    private AudioSource ac;
+	void Start () {
+        ac = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 	// For when the picture is clicked - hopefully
+	private void OnMouseDown() {
+        ac.Play();
+	}
+}
