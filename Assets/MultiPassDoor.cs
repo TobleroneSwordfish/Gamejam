da0ab513b6b9f5c0207b24d97c7b5d1a454ba70e
Sharlene <sd17085@my.bristol.ac.uk>
2018-11-20 00:10:48 +0000

    lots of progress

---
+++ Assets/MultiPassDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class MultiPassDoor : PassDoor
+{
+    protected override string StringParser(string input)
+    {
+        if (input == "aXQncyBhIHNlY3JldA==" || input == "aXRzIGEgc2VjcmV0" || input == "SXQncyBhIHNlY3JldA" || input == "SXRzIGEgc2VjcmV0" || input == "SXQncyBBIFNlY3JldA==" || input == "SXRzIEEgU2VjcmV0")
+            input = "aXQncyBhIHNlY3JldA==";
+        return input;
+    }
+}
